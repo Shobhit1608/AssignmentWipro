4eacb08a915dd45e5ed99dac3706f45334426381
Shobhit1608 <shobhitno1@gmail.com>
2025-08-25 17:29:08 +0530

    Added all projects

--- Quiz-App/src/main/java/com/wipro/shobhit/spring/quizapp/monorepo/enums/DifficultyLevel.java
+++
@@ -1,8 +0,0 @@
-package com.wipro.shobhit.spring.quizapp.monorepo.enums;
-
-public enum DifficultyLevel {
-
-	EASY,
-	MEDIUM,
-	HARD
-}
--- hibernate.schoolApp/src/main/java/com/wipro/shobhit/hibernate/schoolapp/entity/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.wipro.shobhit.hibernate.schoolapp.entity;
-
-public enum Gender {
-    MALE, FEMALE, OTHER
-}
